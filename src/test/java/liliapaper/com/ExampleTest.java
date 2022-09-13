43c5f296fb9e32bd3f23d933b5580bb63b636047
Lil <liliapaper@gmail.com>
2022-09-13 20:38:11 +0300

    Added first test

---
+++ src/test/java/liliapaper/com/ExampleTest.java
@@ -0,0 +1,12 @@
+package liliapaper.com;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void someTest(){
+        Assertions.assertTrue(5>1);
+    }
+}
